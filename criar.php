34a424ab5836521e808b2b0e4b048909ffa7f7f6
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-04-27 10:32:31 -0300

    Inicio projeto

---
+++ criar.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Cadastrar Funcionario</title>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wapper {
+            width: 800px;
+            margin: auto;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="conteiner-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Criar Funcionario</h2>
+                    <form action="criar.php" method="post">
+                        <div class="form-group">
+                            <div>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
